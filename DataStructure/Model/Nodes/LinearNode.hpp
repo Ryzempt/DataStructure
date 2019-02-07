36f11f06250ce5b86740a248e9da750d7813ad60
Robbie <robbiekaufman0504@gmail.com>
2019-02-07 09:29:36 -0700

    Created Linear Node

--- DataStructure/Model/Nodes/LinearNode.hpp
+++ DataStructure/Model/Nodes/LinearNode.hpp
@@ -9,5 +9,52 @@
 #ifndef LinearNode_hpp
 #define LinearNode_hpp
 
+#include "Node.hpp"
+
+teplate <class Type>
+class LinearNode : public Node<Type>
+{
+protected:
+    LinearNode<Type> * next;
+    
+public:
+        //constructors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    //Methods
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
 
 #endif /* LinearNode_hpp */
